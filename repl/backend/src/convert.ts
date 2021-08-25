1a624d55694f35c08046ce864c995dd9a8fe8ca5
Michael Boyles <michael@boyl.es>
2021-08-25 22:15:14 +0100

    feature: select ES target version in REPL

--- repl/backend/src/convert.ts
+++ repl/backend/src/convert.ts
@@ -26,10 +26,10 @@ const CORS_HEADERS = {
     'Access-Control-Allow-Methods': 'GET,POST,DELETE,PATCH'
 };
 
-async function tsCompile(source: string): Promise<string> {
+async function tsCompile(source: string, target: ts.ScriptTarget): Promise<string> {
     const compilerOptions: ts.CompilerOptions = {
         module: ts.ModuleKind.ES2020,
-        target: ts.ScriptTarget.ES2020
+        target
     }
 
     // https://www.typescriptlang.org/dev/typescript-vfs/
@@ -54,12 +54,34 @@ async function tsCompile(source: string): Promise<string> {
 }
 
 export const handler: Handler<ApiGatewayRequest, ApiGatewayResponse> = async (event: ApiGatewayRequest, _ctx) => {
-    const code = decodeURIComponent(event.queryStringParameters.code);
-
-    const response: ApiGatewayResponse = {
-        statusCode: 200,
-        body: await tsCompile(code),
-        headers: CORS_HEADERS
-    };
-    return response;
+    try {
+        const code = decodeURIComponent(event.queryStringParameters.code);
+        const target = convertTarget(decodeURIComponent(event.queryStringParameters.target));
+    
+        return getResponse(200, await tsCompile(code, target));
+    }
+    catch (err) {
+        const msg = err?.message ?? 'Server error';
+        return getResponse(500, msg);
+    }
 };
+
+function convertTarget(target: string) {
+    switch (target) {
+        case 'ES2020': return ts.ScriptTarget.ES2020;
+        case 'ES2019': return ts.ScriptTarget.ES2019;
+        case 'ES2018': return ts.ScriptTarget.ES2018;
+        case 'ES2017': return ts.ScriptTarget.ES2017;
+        case 'ES2016': return ts.ScriptTarget.ES2016;
+        case 'ES2015': return ts.ScriptTarget.ES2015;
+        case 'ES5':    return ts.ScriptTarget.ES5;
+        case 'ES3':    return ts.ScriptTarget.ES3;
+    }
+    throw Error("Unsupported target: " + target);
+}
+
+function getResponse(statusCode: number, body: string): ApiGatewayResponse {
+    return {
+        statusCode, body, headers: CORS_HEADERS
+    };
+}
--- repl/frontend/src/Editor.tsx
+++ repl/frontend/src/Editor.tsx
@@ -4,10 +4,12 @@ import { DiffEditor, Monaco } from "@monaco-editor/react";
 import { useCallback, useState } from "react";
 import { debounce } from "lodash";
 import { SampleSelect } from './SampleSelect';
+import { TargetSelect, Target } from './TargetSelect';
 
-const apiUrl = 'https://abgal4ldi5.execute-api.eu-west-2.amazonaws.com/default';
+const apiUrl = 'https://f5dvdbmccb.execute-api.eu-west-2.amazonaws.com/default';
 
 export const Editor = () => {
+    const [target, setTarget] = useState<Target>('ES2020');
     const [leftText, setLeftText] = useState('');
     const [rightText, setRightText] = useState('');
     const [isFetching, setIsFetching] = useState(false);
@@ -19,14 +21,14 @@ export const Editor = () => {
         });
     }
 
-    const fetchCodeNow = useCallback(async (newSource: string) => {
+    const fetchCodeNow = useCallback(async (target: string, newSource: string) => {
         setIsFetching(true);
 
-        const resp = await fetch(`${apiUrl}/convert?code=${encodeURIComponent(newSource)}`)
+        const resp = await fetch(`${apiUrl}/convert?target=${target}&code=${encodeURIComponent(newSource)}`)
+        setIsFetching(false);
         if (resp.ok) {
             const text = await resp.text();
             setRightText(text);
-            setIsFetching(false);
         }
     }, [setIsFetching, setRightText]);
 
@@ -42,7 +44,8 @@ export const Editor = () => {
 
     return (
         <>
-            <SampleSelect onChange={sample => { setLeftText(sample.source); fetchCodeNow(sample.source); }} />
+            <TargetSelect initialTarget={target} onChange={target => { setTarget(target); fetchCodeNow(target, leftText) } } />
+            <SampleSelect onChange={sample => { setLeftText(sample.source); fetchCodeNow(target, sample.source); }} />
             <button onClick={formatCode}>Format</button>
             <DiffEditor
                 height="90vh"
